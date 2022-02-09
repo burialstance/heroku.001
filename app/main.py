bb3ea6bd8ed7e29568b938a281305a1bc52c082e
burialstance <54133423+burialstance@users.noreply.github.com>
2022-02-10 03:22:02 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ async def get(url):
     async with aiohttp.ClientSession() as client:
         async with client.get(url) as response:
             status_code = response.status
-            return dict(code=status_code)
+            return {k:str(v) for k,v in dir(response)}
 
 @app.get('/ip/{host}')
 async def fetch_ip(request: Request, host: str):
