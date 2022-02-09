b363ab175c5b1c3109e2cb30d069aa19dcee8cbe
burialstance <burialstance@gmail.com>
2022-02-09 16:00:07 +0500

    asdf

--- app/main.py
+++ app/main.py
@@ -7,11 +7,16 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static/"), name="static")
 
-
 templates = Jinja2Templates(directory="app/templates/")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     error = {"title": "errorTitle", "message": "errorMessage"}
-    return templates.TemplateResponse("index.html", {"request": request, "error":error})
+    return templates.TemplateResponse("index.html", {"request": request, "error": error})
+
+
+@app.post('/search', response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    items = [1, 2, 3, 4, 5]
+    return templates.TemplateResponse('index.html', {"request": request, 'items': items})
