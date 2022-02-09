f15ce473dffe2a1d4bd537b2f157b9ec3a9deeee
burialstance <54133423+burialstance@users.noreply.github.com>
2022-02-09 15:18:00 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("app/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="/app/static"), name="static")
 
 
 templates = Jinja2Templates(directory="app/templates")
