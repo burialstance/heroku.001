21a5f9a6ac9cb54ce81a56a7038de6c9b884beb7
burialstance <54133423+burialstance@users.noreply.github.com>
2022-02-23 21:30:18 +0500

    Update coinglass.py

--- coinglass.py
+++ coinglass.py
@@ -1,9 +1,15 @@
-import aiohttp 
+import asyncio
+import aiohttp
 
 
-async def fetch_data(period: str = "5m"):
+async def fetch_data(client, period: str = "5m"):
     url = f"https://fapi.binance.com/futures/data/topLongShortPositionRatio?symbol=BTCUSDT&period={period}"
     async with client.get(url) as resp:
         return await resp.json()
 
-    
+
+async def iter_data(period: str = "5m"):
+    async with aiohttp.ClientSession() as client:
+        while True:
+            yield await fetch_data(client, period=period)
+            asyncio.sleep(1)
