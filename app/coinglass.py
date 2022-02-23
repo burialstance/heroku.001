32347ff3a7c4dfbec4c6a4f31af11633f5484d3d
burialstance <54133423+burialstance@users.noreply.github.com>
2022-02-23 21:47:25 +0500

    Create coinglass.py

---
+++ app/coinglass.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+from fastapi import APIRouter
+
+router = APIRouter()
+
+async def fetch_data(client, period: str = "5m"):
+    url = f"https://fapi.binance.com/futures/data/topLongShortPositionRatio?symbol=BTCUSDT&period={period}"
+    async with client.get(url) as resp:
+        return await resp.json()
+
+
+async def get_data(period: str = "5m"):
+    async with aiohttp.ClientSession() as client:
+        return await fetch_data(client, period=period)
+           
