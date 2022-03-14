ec434023db8cc690f9360df78cdfba777ed5ff46
burialstance <54133423+burialstance@users.noreply.github.com>
2022-03-15 01:47:44 +0500

    Update coinglass.py

--- app/coinglass.py
+++ app/coinglass.py
@@ -24,13 +24,13 @@ class CoinData(BaseModel):
 
 router = APIRouter()
 
-async def fetch_data(client, period: str = "5m"):
-    url = f"https://fapi.binance.com/futures/data/topLongShortPositionRatio?symbol=BTCUSDT&period={period}"
+async def fetch_data(client, coin: str = "btc", period: str = "5m"):
+    url = f"https://fapi.binance.com/futures/data/topLongShortPositionRatio?symbol={coin.upper()}USDT&period={period}"
     async with client.get(url) as resp:
         return await resp.json()
 
 
-async def get_data(period: str = "5m"):
+async def get_data(coin: str = "btc", period: str = "5m"):
     async with aiohttp.ClientSession() as client:
-        return [CoinData(**i) for i in await fetch_data(client, period=period)]
+        return [CoinData(**i) for i in await fetch_data(client, coin, period)]
            
