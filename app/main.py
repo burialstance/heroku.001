70413a49f51905c292490c68a7b05ccb10249075
burialstance <54133423+burialstance@users.noreply.github.com>
2022-03-24 22:21:48 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,71 +1,47 @@
-import random
-import aiohttp
-import asyncio
-from fastapi import FastAPI, Request, Form, WebSocket
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
-from app.coinglass import get_data
-app = FastAPI(debug=True)
+from app.db import DATABASE_URL
+from app.models import UserDB
+from app.users import (
+    auth_backend,
+    current_active_user,
+    fastapi_users,
+    google_oauth_client,
+)
 
-app.mount("/static", StaticFiles(directory="app/static/"), name="static")
+app = FastAPI()
 
-templates = Jinja2Templates(directory="app/templates/")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request, coin: str = "btc"):
-    error = None
-    items = (await get_data(coin=coin))[::-1]
-    coins = {
-        "current": coin,
-        "all": [
-            "BTC",
-            "ETH", 
-            "PEOPLE",
-        ],
-    }
-    context = {
-        "coins": coins,
-        "request": request,
-        "error": error,
-        "items": items,
-        "long": round(items[0].longAccount * 100, 3),
-        "short": round(items[0].shortAccount  * 100, 3),
-    }
-    return templates.TemplateResponse("index.html", context)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(fastapi_users.get_register_router(), prefix="/auth", tags=["auth"])
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
+app.include_router(
+    fastapi_users.get_oauth_router(google_oauth_client, auth_backend, "SECRET"),
+    prefix="/auth/google",
+    tags=["auth"],
+)
 
 
-@app.get('/search', response_class=HTMLResponse)
-async def search(request: Request, query: str):
-    items = [f"{i} {query}" for i in range(20)]
-    context = {
-        'request': request,
-        'items': items,
-        'query': query
-    }
-    return templates.TemplateResponse('index.html', context)
-
-async def get(url):
-    async with aiohttp.ClientSession() as client:
-        async with client.get(url) as response:
-            status_code = response.status
-            return {k: str(getattr(response, k)) for k in dir(response)}
-
-@app.get('/ip/{host}')
-async def fetch_ip(request: Request, host: str):
-    r = await get(f"http://{host}")
-    context = {
-        **r,
-        
-    }
-    return context
+@app.get("/authenticated-route")
+async def authenticated_route(user: UserDB = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+register_tortoise(
+    app,
+    db_url=DATABASE_URL,
+    modules={"models": ["app.models"]},
+    generate_schemas=True,
+)
