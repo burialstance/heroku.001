17d6874e1745c5c38eb1619f1a32c309d0f9b898
burialstance <54133423+burialstance@users.noreply.github.com>
2022-03-26 21:26:46 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, Request, status
 from fastapi.templating import Jinja2Templates
 
 from app.users import current_active_user
@@ -9,5 +9,5 @@ templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
 @router.get("/")
-async def web_index():
+async def web_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
