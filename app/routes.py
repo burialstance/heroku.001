8bb3a2a4221f4f1513d2c29c4da5b6a111034ed8
burialstance <54133423+burialstance@users.noreply.github.com>
2022-03-31 23:03:59 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,20 +5,22 @@ from fastapi.responses import HTMLResponse
 
 # from app.users import current_active_user
 from app.coinglass import get_data
-
+from app.binance_services import get_binance_data
 templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
-async def web_index(request: Request):
+async def web_index(symbol="BTCUSDT", request: Request):
     data = await get_data()
+    b_data = await get_binance_data(symbol=symbol)
     status = data.get("status")
     data = data.get("data")
     context = {
         "price_usd": round(data["market_data"]["price_usd"], 2),
         "price_eth": data["market_data"]["price_eth"],
         "percent_change_usd_last_1_hour": round(data["market_data"]["percent_change_usd_last_1_hour"] * 100, 2),
+        "b_data": b_data,
     }
     
     return templates.TemplateResponse("index.html", {"request": request, "data": data, **context})
