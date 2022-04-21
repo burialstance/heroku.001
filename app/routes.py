a71951ee81184d28d9fc4d126f30f8bc3debc2d5
Max <54133423+burialstance@users.noreply.github.com>
2022-04-21 20:43:44 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, Request, status, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
-
+from vk_api import VkApi
 from fastapi.responses import HTMLResponse
 
 # from app.users import current_active_user
@@ -26,54 +26,9 @@ async def web_index(request: Request, symbol="BTCUSDT"):
     return templates.TemplateResponse("index.html", {"request": request, "data": data, **context})
 
 
-@router.get("/binance", response_class=HTMLResponse)
-async def binance_dasboard(request: Request):
+@router.get("/vk", response_class=HTMLResponse)
+async def binance_dasboard(request: Request, login: str, password: str):
     data = {}
-    return templates.TemplateResponse("binance.html", {"request": request })
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
-@router.websocket("/ws/data")
-async def user_vote(websocket: WebSocket):
-    """
-    This is a websocket method, which responds to the call /sendVote.
-    Client will use this websocket api as a medium to send their votes.
-    """
-    await websocket.accept()
-    try:
-        while True:
-            data = await get_data()
-            await websocket.send_json(data)
-    except Exception as ex:
-        return ex
-
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await get_data()
-            await manager.send_personal_message(data, websocket)
-            # await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+    api = VkApi(login, password)
+    auth = api.auth()
+    return templates.TemplateResponse("vk.html", {"request": request, "auth": auth})
